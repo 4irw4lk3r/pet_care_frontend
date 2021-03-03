e01edb44ea49cf193bc171d224685c9a6c788c96
4irw4lk3r <federico.delgado@gmail.com>
2021-03-02 21:23:42 -0300

    Adicionando Material Design Icons

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <nav>
     <div class="nav-wrapper green lighten-3">
-      <a href="#" class="brand-logo">Logo</a>
+      <a href="#" class="brand-logo">Remember my Dogs!!</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
+        <li><a href="sass.html"><i class="mdi mdi-36px mdi-dog"></i></a></li>
+        <li><a href="badges.html"><i class="mdi mdi-36px mdi-face"></i></a></li>
+        <li><a href="collapsible.html"><i class="mdi mdi-36px mdi-briefcase-plus"></i></a></li>
       </ul>
     </div>
   </nav>
